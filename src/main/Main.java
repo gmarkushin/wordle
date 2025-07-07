3db07f75b35ef5f7985d2404f9ef2b63218a77cd
Grigoriy <cocs21cs@gmail.com>
2025-07-07 11:18:06 +0300

    repush

--- src/main/Main.java
+++
@@ -1,8 +0,0 @@
-package main;
-import main.api.Api;
-public class Main {
-    public static void main(String[] args) {
-        Api api = new Api();
-        api.gameProc();
-    }
-}
--- src/main/api/Sender.java
+++
@@ -1,9 +0,0 @@
-package main.api;
-public class Sender{
-	public void send(String mess){
-		System.out.println(mess);
-	}
-	public void send(char[] mess){
-		System.out.println(mess);
-	}
-}
